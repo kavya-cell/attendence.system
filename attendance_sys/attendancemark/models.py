6ea16b7f052838bf5852356e8176fadf9843ea2b
keerthanar09 <keerthana241495@gmail.com>
2024-11-11 17:34:05 +0530

    Created database student and attendance in django

--- attendance_sys/attendancemark/models.py
+++ attendance_sys/attendancemark/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+#Table that contains students information, along with their images that will be used for processing.
+class Student(models.Model):
+    rollnum = models.CharField(max_length=10, primary_key=True)
+    name = models.CharField(max_length=100)
+    branch = models.CharField(max_length=40)
+    year = models.IntegerField()
+    image = models.ImageField()
+
+#Table to mark the attendance of students.
+class Attendance(models.Model):
+    slno = models.AutoField(primary_key=True)
+    rollnum = models.ForeignKey(Student, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    status = models.BooleanField()
+    

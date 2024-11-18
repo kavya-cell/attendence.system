1531a372d6e85ed18a9e25ead48fd84631d5d311
keerthanar09 <keerthana241495@gmail.com>
2024-11-18 17:19:38 +0530

    Commit for small changes

--- attendance_sys/attendancemark/face rec.py
+++ attendance_sys/attendancemark/face rec.py
@@ -1,15 +1,15 @@
 import face_recognition
 import cv2
 import numpy as np
-known_image_1 = face_recognition.load_image_file("known_person_1.jpg")
+known_image_1 = face_recognition.load_image_file("static/attendancemark/known_person_1.png")
 known_image_1_encoding = face_recognition.face_encodings(known_image_1)[0]
 
-known_image_2 = face_recognition.load_image_file("known_person_2.jpg")
-known_image_2_encoding = face_recognition.face_encodings(known_image_2)[0]
+# known_image_2 = face_recognition.load_image_file("static/attendancemark/known_person_2.png")
+# known_image_2_encoding = face_recognition.face_encodings(known_image_2)[0]
 
-known_face_encodings = [known_image_1_encoding, known_image_2_encoding]
+known_face_encodings = [known_image_1_encoding]
 known_face_names = ["Person 1", "Person 2"]
-unknown_image = face_recognition.load_image_file("unknown.jpg")
+unknown_image = face_recognition.load_image_file("static/attendancemark/unknown.jpg")
 face_locations = face_recognition.face_locations(unknown_image)
 face_encodings = face_recognition.face_encodings(unknown_image, face_locations)
 
